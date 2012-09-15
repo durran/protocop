fcca56ae7051f8f40fba373ba30b805d75413272
Durran Jordan <durran@gmail.com>
2012-09-15 15:04:44 +0200

    Add encodable/decodable spec templates

---
+++ spec/protocop/decodable_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+describe Protocop::Decodable do
+
+  describe ".decode" do
+
+  end
+end
---
+++ spec/protocop/encodable_spec.rb
@@ -0,0 +1,8 @@
+require "spec_helper"
+
+describe Protocop::Encodable do
+
+  describe "#encode" do
+
+  end
+end
