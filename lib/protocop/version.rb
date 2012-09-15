20bd03dc1b8c45d027c32afd6c99616dabc79860
Durran Jordan <durran@gmail.com>
2012-09-15 12:37:58 +0200

    Adding in version constant

---
+++ lib/protocop/version.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+module Protocop
+
+  # Constant for the current version of the library. Protocop adheres to the
+  # Semantic Versioning Specification defined at http://semver.org/
+  VERSION = "0.0.0"
+end
