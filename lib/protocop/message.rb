975bca37a1bbc8c398728c21f35ac442b7d3dd2f
Durran Jordan <durran@gmail.com>
2012-09-15 12:46:03 +0200

    Get the message module in

---
+++ lib/protocop/message.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+module Protocop
+
+  # A Message is simply an encodable structure that conforms to Google's
+  # Protocol Buffer specification.
+  #
+  # @see http://code.google.com/p/protobuf/
+  module Message
+
+  end
+end
