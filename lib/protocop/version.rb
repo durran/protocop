745bd8368937163361eee0bf2e5a8fe9588b322b
Durran Jordan <durran@gmail.com>
2012-09-15 14:11:21 +0200

    Update constant rdoc

--- lib/protocop/version.rb
+++ lib/protocop/version.rb
@@ -4,6 +4,8 @@ module Protocop
   # Constant for the current version of the library. Protocop adheres to the
   # Semantic Versioning Specification defined at http://semver.org/
   #
+  # @return [ String ] The version in the format "major.minor.patch".
+  #
   # @since 0.0.0
   VERSION = "0.0.0"
 end
--- lib/protocop/wire.rb
+++ lib/protocop/wire.rb
@@ -11,10 +11,14 @@ module Protocop
     # Includes the types: int32, int64, uint32, uint64, sint32, sint64, bool,
     # enum.
     #
+    # @return [ Integer ] 0.
+    #
     # @since 0.0.0
     VARINT = 0
 
     # Includes the types: fixed64, sfixed64, double.
+    #
+    # @return [ Integer ] 1.
     #
     # @since 0.0.0
     BIT64 = 1
@@ -22,10 +26,14 @@ module Protocop
     # Includes the types: string, bytes, embedded messages, packed repeated
     # fields.
     #
+    # @return [ Integer ] 2.
+    #
     # @since 0.0.0
     LENGTH = 2
 
     # Includes the types: fixed32, sfixed32, float.
+    #
+    # @return [ Integer ] 5.
     #
     # @since 0.0.0
     BIT32 = 5
