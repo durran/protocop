97b3bee50b618189cb240cc6fdb919eba65a03fc
Durran Jordan <durran@gmail.com>
2012-10-27 19:30:52 +0200

    Add fields for uint32 and uint64

---
+++ spec/protocop/fields/uint64_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe Protocop::Fields::Uint64 do
+
+  let(:field) do
+    described_class.new(1)
+  end
+
+  let(:buffer) do
+    Protocop::Buffer.new
+  end
+
+  describe "#encode" do
+
+    let!(:written) do
+      field.encode(buffer, 150)
+    end
+
+    it "encodes the field, type and integer" do
+      expect(buffer.bytes).to eq("\x08\x96\x01")
+    end
+
+    it_behaves_like "a fluid interface"
+  end
+end
