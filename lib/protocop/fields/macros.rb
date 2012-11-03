6f9dd561e04b3855fc3d7a7150d7dd39c4f0c4d8
Durran Jordan <durran@gmail.com>
2012-11-03 11:43:53 +0100

    Add optional fields

--- lib/protocop/fields/macros.rb
+++ lib/protocop/fields/macros.rb
@@ -21,6 +21,47 @@ module Protocop
         @fields ||= {}
       end
 
+      # Define an optional field in the message.
+      #
+      # @example Define an optional field.
+      #   class Request
+      #     include Protocop::Message
+      #     optional :string, :name, 1
+      #   end
+      #
+      # @example Define an optional enum field.
+      #   class Request
+      #     include Protocop::Message
+      #     module Type
+      #       QUERY = 0
+      #       COUNT = 1
+      #     end
+      #     optional Type, :type, 1, default: Type::QUERY
+      #   end
+      #
+      # @example Define an embedded message field.
+      #   class Command
+      #     include Protocop::Message
+      #     optional :string, :name, 1
+      #   end
+      #
+      #   class Request
+      #     include Protocop::Message
+      #     optional Command, :command, 2
+      #   end
+      #
+      # @param [ Class, Module, Symbol ] type The field's type.
+      # @param [ Symbol ] name The name of the field.
+      # @param [ Integer ] number The field's identifier for encoding/decoding.
+      # @param [ Hash ] options The field options.
+      #
+      # @option options [ Integer ] :default The default value.
+      #
+      # @since 0.0.0
+      def optional(type, name, number, options = {})
+        define_field(type, name, number, options)
+      end
+
       # Define a required field in the message.
       #
       # @example Define a required field.
@@ -60,11 +101,31 @@ module Protocop
       # @since 0.0.0
       def required(type, name, number, options = {})
         frame_opts = options.merge(required: true)
-        fields[name] = type.__protofield__(type, name, number, frame_opts)
-        field_accessors(name)
+        define_field(type, name, number, frame_opts)
       end
 
       private
+
+      # Defines a field in the message.
+      #
+      # @api private
+      #
+      # @example Define the field.
+      #   Message.define_field(:string, :test, 1)
+      #
+      # @param [ Class, Module, Symbol ] type The field's type.
+      # @param [ Symbol ] name The name of the field.
+      # @param [ Integer ] number The field's identifier for encoding/decoding.
+      # @param [ Hash ] options The field options.
+      #
+      # @option options [ Integer ] :default The default value.
+      # @option options [ Boolean ] :requires If the field is required.
+      #
+      # @since 0.0.0
+      def define_field(type, name, number, options = {})
+        fields[name] = type.__protofield__(type, name, number, options)
+        field_accessors(name)
+      end
 
       # Define the accessors (getter and setter) for the field.
       #
--- spec/protocop/fields/macros_spec.rb
+++ spec/protocop/fields/macros_spec.rb
@@ -2,6 +2,59 @@ require "spec_helper"
 
 describe Protocop::Fields::Macros do
 
+  describe ".optional" do
+
+    before(:all) do
+      class Request
+        include Protocop::Message
+        optional :string, :name, 1
+      end
+    end
+
+    after(:all) do
+      Object.__send__(:remove_const, :Request)
+    end
+
+    let(:field) do
+      Request.fields[:name]
+    end
+
+    it "adds the field to the class" do
+      expect(field).to be_a(Protocop::Fields::String)
+    end
+
+    it "sets the field number" do
+      expect(field.number).to eq(1)
+    end
+
+    it "does not require the field" do
+      expect(field).to_not be_required
+    end
+
+    context "when setting the string via the setter" do
+
+      let(:message) do
+        Request.new
+      end
+
+      let!(:string) do
+        message.name = "testing"
+      end
+
+      it "sets the string in the message" do
+        expect(message.name).to eq("testing")
+      end
+
+      it "returns the set string" do
+        expect(string).to eq("testing")
+      end
+
+      it "provides access to the fields from the instance" do
+        expect(message.fields).to eq(Request.fields)
+      end
+    end
+  end
+
   describe ".required" do
 
     context "when providing an embedded message" do
